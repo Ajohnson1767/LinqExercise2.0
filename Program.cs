94e5e44b11b017d6c9d5204f20b71ee2303828ee
Aaron <aaron.johnson35043@gmail.com>
2020-10-27 13:32:23 -0500

    OddorEven Exercise

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace if_else_project
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int favNumber = 200;
+
+
+			string input = Console.ReadLine();
+
+			int userGuess = int.Parse(input);
+
+			if (userGuess == favNumber)
+			{
+				Console.WriteLine($"Correct!");
+			}
+			else if (userGuess > favNumber) 
+			{
+				Console.WriteLine($"Too High!");
+			}
+			else
+			{
+				Console.WriteLine($"Too Low!");
+
+            }
+				
+		
+	
+		
+
