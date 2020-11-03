9bef35dde2999c5495ab8461a415ff1db2183081
Aaron <aaron.johnson35043@gmail.com>
2020-11-03 14:45:11 -0600

    syntax sugar

---
+++ SyntaxSugar.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+
+//namespace ConsoleApp3
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //inferred typing, string interpolation, ternary operator
+
+
+//            int answer = 4;
+
+//            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+//            Console.WriteLine(response);
+
+//            //if (answer < 9)
+//            //{
+//            //    response = answer + " is less than nine";
+//            //}
+//            //else
+//            //{
+//            //    response = answer + “greater than or equal to nine”;
+//            //}
+
+
+
+
+
+
+
+//        }
+//    }
+//}
