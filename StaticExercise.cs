1342a357404ade1bd4509080505f5c6a3a8fa7bb
Aaron <aaron.johnson35043@gmail.com>
2020-11-10 15:36:58 -0600

    added files

---
+++ StaticExercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cel = TempConverter.FahrenheitToCelsius(98);
+
+            Console.WriteLine($"{cel}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(32);
+
+            Console.WriteLine($"{fahrenheit}");
+        }
+    }
+}
